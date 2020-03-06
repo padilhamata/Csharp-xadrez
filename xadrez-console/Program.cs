8ab67584bc9be9490325945f9aefa962ed01fd0b
lucas.mata <lucas.mata@enerwatt.com.br>
2020-03-06 16:36:47 -0300

    Começando o projeto

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -1,12 +1,19 @@
 ﻿using System;
-
+using tabuleiro;
 namespace xadrez_console
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao p;
+
+            p = new Posicao(3, 4);
+
+            Console.WriteLine("Posicao "+ p);
+
+
+
         }
     }
 }
