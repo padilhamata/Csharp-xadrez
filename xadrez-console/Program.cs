0f0c91631ec4c6a81c5eac6d5263e9a235a979b5
lucas.mata <lucas.mata@enerwatt.com.br>
2020-03-09 18:00:08 -0300

    Colocando pecas emjogo

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -12,12 +12,20 @@ namespace xadrez_console
                 PartidaDeXadrez partida = new PartidaDeXadrez();
                 while (!partida.terminada)
                 {
+                    try { 
                     Console.Clear();
                     Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.WriteLine("Turno: " + partida.turno);
+                    Console.WriteLine("Aguardando jogada: " + partida.jogadorAtual);
+
+
+
 
                     Console.WriteLine();
                     Console.Write("Origem: ");
                     Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    partida.validarPosicaoDeOrigem(origem);
 
                     bool[,] posicoesPossiveis = partida.tab.peca(origem).movimentosPossiveis();
 
@@ -26,9 +34,16 @@ namespace xadrez_console
 
                     Console.Write("Destino: ");
                     Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
+                        partida.validarPosicaoDeDestino(origem, destino);
 
-                    partida.executaMovimento(origem, destino);
-                }
+                    partida.realizaJogada(origem, destino);
+                    }
+                    catch(TabuleiroException e)
+                    {
+                        Console.WriteLine(e.Message);
+                        Console.ReadLine();
+                    }
+                    }
 
                 Tela.imprimirTabuleiro(partida.tab);
             }catch(TabuleiroException e)
